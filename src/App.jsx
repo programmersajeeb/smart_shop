59d77581ece655c779dfa0134b7f1ab5b5b5b180
programmersajeeb <sajeeb.programmer@gmail.com>
2025-11-24 23:41:34 +0600

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Smart Shop</h1>
-      </div>
+      <Home />
     </>
   )
 }
