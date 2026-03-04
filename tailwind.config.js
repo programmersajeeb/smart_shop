6b37234be4eadc4b88114578b4d30d23c1503efe
mdibrahim-007 <ibrahim868146@gmail.com>
2026-03-05 00:18:14 +0600

    theme color problem fixed

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // ✅ system theme follow করবে না
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
