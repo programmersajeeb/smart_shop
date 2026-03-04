7e3e7071bf2d13b675e679ff4b5dda0bf05e2898
mdibrahim-007 <ibrahim868146@gmail.com>
2026-03-05 00:26:53 +0600

    theme problem solve

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   // ✅ system theme follow করবে না
   darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  plugins: [daisyui],
+  // ✅ DaisyUI থিম একটাই রাখলে আর auto বদলাবে না
+  daisyui: {
+    themes: ["light"],
+  },
 };
